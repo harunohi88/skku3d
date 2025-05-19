9f67a8e48c1c294a1039eaa0488663132e7a5ea3
okpocandy <opcandy@naver.com>
2025-05-19 16:21:14 +0900

    [Feat] EnemySpawner, BasicEnemyPool, ObjectPool, Boids

---
+++ Assets/02.Scripts/Pool/BasicEnemyPool.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BasicEnemyPool : BehaviourSingleton<BasicEnemyPool>
+{
+    private ObjectPool<AEnemy> _pool;
+    public List<AEnemy> EnemyPrefabs; // 여러 적 프리팹 리스트
+    public int PoolSize = 100;
+
+    private void Awake()
+    {
+        _pool = new ObjectPool<AEnemy>(EnemyPrefabs, PoolSize, transform);
+    }
+
+    public AEnemy Get()
+    {
+        var enemy = _pool.Get();
+        // TODO Init함수 실행
+        return enemy;
+    }
+
+    public void Return(AEnemy enemy)
+    {
+        _pool.Return(enemy);
+    }
+}
