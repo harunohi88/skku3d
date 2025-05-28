fff60352cda384451f1f0070c863677bfa653132
okpocandy <opcandy@naver.com>
2025-05-28 18:30:35 +0900

    [Modify] RuneShop UI

---
+++ Assets/02.Scripts/UpgradeAndShop/SkillUpgrade.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillUpgrade : MonoBehaviour
+{
+    public SkillUpgrade(int index)
+    {
+        Debug.Log($"SkillUpgrade : {index}");
+    }
+}
