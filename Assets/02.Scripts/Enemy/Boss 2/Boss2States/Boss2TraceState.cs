400df959eb0bbd3108469fbf95709ecf7eae7fff
Suhyeon Park <suhyeon12341@gmail.com>
2025-06-04 18:02:43 +0900

    보스 애니메이션

--- Assets/02.Scripts/Enemy/Boss 2/Boss2States/Boss2TraceState.cs
+++ Assets/02.Scripts/Enemy/Boss 2/Boss2States/Boss2TraceState.cs
@@ -7,19 +7,26 @@ public class Boss2TraceState : IState<AEnemy>
     public void Enter(AEnemy enemy)
     {
         Debug.Log(this);
+        _time = 0f;
         enemy.SetAnimationTrigger("Trace");
         //AudioManager.Instance.PlayEnemyAudio(EnemyType.Boss, EnemyAudioType.Boss2Trace);
-        enemy.Agent.isStopped = false;
-        enemy.Agent.ResetPath();
         enemy.EnemyRotation.IsFound = true;
-        //enemy.Agent.SetDestination(PlayerManager.Instance.Player.transform.position);
+        enemy.Agent.SetDestination(PlayerManager.Instance.Player.transform.position);
     }
     public void Update(AEnemy enemy)
     {
-        enemy.Agent.SetDestination(PlayerManager.Instance.Player.transform.position);
+        Debug.Log("보스2 Trace 업데이트 진입");
+        if (enemy.Agent.remainingDistance < enemy.Agent.stoppingDistance)
+        {
+            Debug.Log($"RemaningDistance: {enemy.Agent.remainingDistance}, stoppingDistance: {enemy.Agent.stoppingDistance}");
+            enemy.ChangeState(new Boss2IdleState());
+        }
+
+
         _time += Time.deltaTime;
         if (_time >= 3f)
         {
+            Debug.Log("보스2 Trace 업데이트 진입 3초 지남");
             IState<AEnemy> state = Boss2AIManager.Instance.DecideNextState();
             if (state is Boss2TraceState) return;
             else enemy.ChangeState(state);
